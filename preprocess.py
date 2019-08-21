e30ecb0b824d262d84de5013d73efd61bec1e711
Burtenshaw <ben.burtenshaw@gmail.com>
2019-08-21 12:47:11 +0200

    add data specific debugging

--- preprocess.py
+++ preprocess.py
@@ -4,19 +4,20 @@ import json
 import os
 from datetime import datetime
 from dateutil import parser
-
+import argparse
 #%%
 class whatsApp:
     """ process whatsapp conversations into a json, maintain conversation and user structure
     use 'remove_names' to remove all the names from lines
     use 'save_key' to store a key in each file to return real users. WARNING: Not anonymous. 
     """
-    def __init__(self, data_dir="data/", out_dir="output_file.json", remove_names=True, save_key=False):
+    def __init__(self, data_dir="data/", out_dir="output_file.json", remove_names=True, save_key=False, debug=False):
         self.dir = data_dir
         self.paths = self.load()
         self.out_dir = out_dir
         self.remove_names = remove_names
         self.save_key = save_key
+        self.debug = debug
 
     def load(self):
         return [self.dir + x for x in os.listdir(self.dir) if x[-4:] == ".txt"]
@@ -28,21 +29,25 @@ class whatsApp:
 
     def conversation(self, f):
         _f = open(f)
-        lines = [self.line(l) for l in _f]
-        users, users_seq = self.users(lines)
-
-        if self.remove_names:
-            lines, users_key = self.anon(lines, users)
+        
+        if self.debug:
+            [self.lineDebug(l) for l in _f]
+        else:
+            lines = [self.line(l) for l in _f]
             users, users_seq = self.users(lines)
 
-        date_range = [lines[0]['utc'], lines[-1]['utc']]
+            if self.remove_names:
+                lines, users_key = self.anon(lines, users)
+                users, users_seq = self.users(lines)
 
-        return {"lines":lines, 
-                "user_seq":users_seq, 
-                "users":users, 
-                "date_range":date_range,
-                "source": f,
-                "users_key": users_key}
+            date_range = [lines[0]['utc'], lines[-1]['utc']]
+
+            return {"lines":lines, 
+                    "user_seq":users_seq, 
+                    "users":users, 
+                    "date_range":date_range,
+                    "source": f,
+                    "users_key": users_key}
 
     def anon(self, lines, users):
         users_key = {u:n for n,u in enumerate(users)}
@@ -55,19 +60,43 @@ class whatsApp:
     def line(self, line):
         _line = re.split("\ \-.*\:", line)
         return {"utc":int(parser.parse(_line[0]).timestamp()),
-                   "user":re.search("([A-Z])\w+\:", line).group(0),
-                   "text":_line[1],
-                   "raw":_line}
+                "user":re.search("([A-Z])\w+\:", line).group(0),
+                "text":_line[1],
+                "raw":_line}
 
     def fileIter(self):
         return {n: self.conversation(f) for n, f in enumerate(self.paths)}
     
-    def run_save(self):
+    def run(self):
         data = self.fileIter()
         with open(self.out_dir, 'w') as f:
             json.dump(data,f)
+    
+    def lineDebug(self, line):
+        _line = re.split("\ \-.*\:", line)
+        try:
+            re.split("\ \-.*\:", line)
+        except:
+            print("line split error: ", "\n", "PRIVATE LINE:  ", line)
+        try:
+            int(parser.parse(_line[0]).timestamp())
+        except:
+            print("date error", "\n", "PRIVATE LINE:  ", line)
+        try:
+            re.search("([A-Z])\w+\:", line).group(0)
+        except:
+            print("regex user error", "\n", "PRIVATE LINE:  ", line)
 
 
-w = whatsApp()
+if __name__== "__main__":
 
-w.run_save()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--debug", help="get more info and examples from data errors.", action="store_true")
+    args = argparser.parse_args()
+
+    if args.debug:
+        w = whatsApp(debug=True)
+    else:
+        w = whatsApp()
+
+    w.run()
