8f203d34d1811ae92cc23e7d699cd9ebcaccd146
Burtenshaw <ben.burtenshaw@gmail.com>
2019-08-23 22:02:40 +0200

    all in one regex

--- preprocess.py
+++ preprocess.py
@@ -108,6 +108,37 @@ class instantMessage:
         with open(self.out_dir, 'w') as f:
             json.dump(data,f)
 
+one_pattern = "(?# get date)(?P<date>(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))(?:\:\ |\ \-\ )(?# get user)(?P<user>.+?)(?:\:)(?# get text)(?P<text>(.|\r|\n?|\n)+?(?=(?:(?# next date or end of the doc)(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))|(?# end of doc)\Z))"
+
+class whatsApp_one(instantMessage):
+    def line(self, line, n_line):
+        date, user, text = line['date'], line['user'], line['text']
+        utc = int(parser.parse(date).timestamp())
+        return {"line" : n_line, 
+                "utc":utc, 
+                "user":user, 
+                "text":text, 
+                "raw_date":date}
+    
+    def conversation(self, file):
+        print("parsing file: ", file)
+        r = re.compile(self.pattern)
+        with open(file, 'r', encoding="utf-8") as f:
+            doc = [m.groupdict() for m in r.finditer(f.read())]
+        lines = [self.line(l, n) for n, l in enumerate(doc)]
+        users, users_seq = self.users(lines)
+        if self.remove_names:
+            lines, users_key = self.anon(lines, users)
+            users, users_seq = self.users(lines)
+
+        date_range = [lines[0]['utc'], lines[-1]['utc']]
+        return {"lines":lines, 
+                "user_seq":users_seq, 
+                "users":users, 
+                "date_range":date_range,
+                "source": file,
+                "users_key": users_key}            
+
 class whatsApp(instantMessage):
     pass
 
