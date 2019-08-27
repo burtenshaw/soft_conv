55ca20eeaaa21d45ea1b1055acd7c6110bd2e54f
Burtenshaw <ben.burtenshaw@gmail.com>
2019-08-27 14:51:09 +0200

    add alpha parsing to pre

--- preprocess.py
+++ preprocess.py
@@ -30,6 +30,7 @@ class instantMessage:
         return [self.dir + x for x in os.listdir(self.dir) if x[-4:] == ".txt"]
 
     def users(self, lines):
+        # TODO rewrite to encorporte alpha user key
         users_seq = [l['user'] for l in lines]
         users = list(set(users_seq))
         return [users, users_seq]
@@ -41,9 +42,12 @@ class instantMessage:
         if not self.save_key:
             users_key={}
         return lines, users_key
-
+    
+    def anon_filenames(self,filename):
+        return filename
+    
     def fileIter(self):
-        return {n: self.conversation(f) for n, f in enumerate(self.paths)}
+        return {self.conv_n: self.conversation(f) for self.conv_n, f in enumerate(self.paths)}
     
     def line(self, line, n_line):
         date, user, text = line['date'], line['user'], line['text']
@@ -51,7 +55,8 @@ class instantMessage:
             utc = int(parser.parse(date).timestamp())
         except:
             utc: None
-        return {"line" : n_line, 
+        return {"conv_n":self.conv_n,
+                "line_n" : n_line, 
                 "utc":utc, 
                 "user":user, 
                 "text":text, 
@@ -76,13 +81,42 @@ class instantMessage:
                 "users":users, 
                 # "date_range":date_range,
                 "source": file,
-                "users_key": users_key}        
+                "users_key": users_key}  
     
+    def user_data(self):
+        return {user:{'lines': [[line['conv_n'], line['line_n'], line['utc']] 
+                       for line in self.lines_d]} 
+                       for user in self.users(self.lines_d)[0]}
+    
+    def line_data(self):
+        return [line for _, conv in self.data.items() for line in conv['lines']]
+    
+    def conversations_data(self):
+        return [[l['line_n'] for l in conv['lines']] for _, conv in self.data.items()]
+    
+    def separate_data(self):
+        self.lines_d = self.line_data()
+        self.conversations_d = self.conversations_data()
+        self.user_d = self.user_data()
+        for d in [['lines',self.lines_d], 
+                  ['conversations',self.conversations_d], 
+                  ['users',self.user_d]]:
+            self.out_dir = d[0] + ".json"
+            self.data = d[1]
+            self.save()
+            
     def run(self):
-        """ define approach and run """
-        data = self.fileIter()
+        print("iterating over files")
+        self.data = self.fileIter()
+        print("saving data")
+        self.save()
+        
+    def save(self):
         with open(self.out_dir, 'w') as f:
-            json.dump(data,f)
+            json.dump(self.data,f)
+#         self.csv = True
+#         if self.csv:
+#             self.make_csv()
 
 class lineByline(instantMessage):
 
@@ -101,26 +135,21 @@ class lineByline(instantMessage):
             except:
                 print("Broken pattern")
                 pass
-    
-    def line(self, line, n_line):
-        try:
-            line = re.match(self.pattern, line).groupdict()
-            self.make_line(line, n_line)
-        except:
-            return str(line)
 
     def conversation(self, file):
         print("parsing file: ", file)
         doc = list(open(file, 'r', encoding="utf-8"))
         self.validate_date_pattern(doc[0])
+        
         lines = {}
         for n, l in enumerate(doc):
-            _l = self.line(l, n)
-            if type(_l) == dict:
+            try:
+                self.line(re.match(self.pattern, line).groupdict(), n)
                 lines[n] = _l
                 x = n
-            elif type(_l) == str:
+            except:
                 lines[x]['text'] += _l
+                
         lines = [l[1] for l in lines.items()]  
         users, users_seq = self.users(lines)
         if self.remove_names:
@@ -139,12 +168,92 @@ class lineByline(instantMessage):
 class whatsApp(instantMessage):
     pass
 
-class facebook(instantMessage):
-    pass
+class facebook(instantMessage):    
+    def regex_file(self, file):
+        print("parsing file: ", file)
+        r = re.compile(self.pattern['conv'])
+        with open(file, 'r', encoding="utf-8") as f:
+            return ''.join([m.groupdict()['exchange'] for m in r.finditer(f.read())])
 
+    def conversation(self, file):
+        f = self.regex_file(file)
+        r = re.compile(self.pattern['line'])
+        doc = [m.groupdict() for m in r.finditer(f)]
+        lines = [self.line(l, n) for n, l in enumerate(doc)]
+        self.lines = lines    
+        users, users_seq = self.users(lines)
+        if self.remove_names:
+            lines, users_key = self.anon(lines, users)
+            users, users_seq = self.users(lines)
+
+        date_range = [lines[0]['utc'], lines[-1]['utc']]
+        return {"lines":lines, 
+                "user_seq":users_seq, 
+                "users":users, 
+                "date_range":date_range,
+                "source": file,
+                "users_key": users_key}  
+
+class alphaData:
+    def __init__(self, src_path, names):
+        self.src = src_path
+        self.names = names
+        self.load()
+
+    def load(self):
+        self.df = pd.read_csv(file_path, delimiter='\t', names=names['all'])
+        
+    def make_user_df(self):
+        user_df = self.df[names['user_fields']]
+        user_df = user_df.drop_duplicates(subset='chatter_ID', keep='first')
+        user_df = user_df.set_index('chatter_ID')
+        
+        gp = self.df.groupby('chatter_ID', sort=False)
+        user_df['lines_n'] = pd.Series(dict(gp.groups))
+        
+        self.user_df = user_df
+
+    def make_line_df(self):
+        self.line_df = self.df[names['line_fields']]
+    
+    def run(self):
+        self.make_line_df()
+        self.make_user_df()
+    
+    def save(self, output_dir):
+        self.line_df.to_csv(output_dir+"alpha_lines.csv")
+        self.user_df.to_csv(output_dir+"alpha_users.csv")
+
+names = {
+        'all': ["post", "chatter_ID", "medium", "year", "conv_sex", "conv_size", "gender", "age", "region", "education", "language", "prof", "prof_cat", "permission", "permission_parents", "school", "in_gesprek"],
+        'line_fields' : [
+        "post",
+        "chatter_ID",
+        "medium",
+        "year",
+        "conv_sex",
+        "conv_size"
+                ],
+         'user_fields' : [
+        "chatter_ID",
+        "gender",
+        "age",
+        "region",
+        "education",
+        "language",
+        "prof",
+        "prof_cat",
+        "permission",
+        "permission_parents",
+        "school",
+        "in_gesprek"
+         ]
+        }
+        
 patterns = {"date": ["(?P<date>(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))(?:\:\ |\ \-\ )(?P<user>.+?)(?:\:)(?P<text>.*)"],
-            "one" : "(?# get date)(?P<date>(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))(?:\:\ |\ \-\ )(?# get user)(?P<user>.+?)(?:\:)(?# get text)(?P<text>(.|\r|\n?|\n)+?(?=(?:(?# next date or end of the doc)(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))|(?# end of doc)\Z))"
-}
+            "one" : "(?# get date)(?P<date>(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))(?:\:\ |\ \-\ )(?# get user)(?P<user>.+?)(?:\:)(?# get text)(?P<text>(.|\r|\n?|\n)+?(?=(?:(?# next date or end of the doc)(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))|(?# end of doc)\Z))",
+            'conv':"(?# get conversation date marker)(?P<conv>(?P<fb_date>(?P<_day>\d\d)(?:\s)(?P<_month>(?:[a-z|A_Z]{4,10}))\s(?P<_year>(?:[0-9]{4}))))(?:\n.*\n)(?P<exchange>(?:.*|\r|\n?|\n)+?(?=(?:(?:\d\d)\s(?:(?:[a-z|A_Z]{4,10}))\s(?:(?:[0-9]{4}))|\Z)))",
+           'line': '(?# get the actual date)(?P<date>(?P<day>\d\d)\-(?P<month>(?:[0-2][0-9]))\-(?P<year>(?:[0-9]{4}))\s(?P<time>[0-9][0-9]\:[0-9][0-9]))\n(?# username)(?P<user>.*)\n(?# text)(?P<text>(?:.*|\r|\n?|\n)+?(?=(?:(?P<receiver>.*\n){1}(?# next date or end of the doc)(?:(?:[0-3][0-9])|(?:[0-9]))(?:\/|\-|\.)(?:(?:(?:0)[0-9])|(?:(?:1)[0-2]))(?:\/|\-|\.)(?:\d{2}|\d{4})(?:,|) (?:[0-2][0-9])\:(?:[0-5][0-9])(?:pm|am| pm| am|\:[0-5][0-9]|))|(?# end of doc)\Z))'}
 
 if __name__== "__main__":
 
